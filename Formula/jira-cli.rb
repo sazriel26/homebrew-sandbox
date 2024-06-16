e7362ba2855851652414b81e7c9c10533ecda355
Sharon Azriel <150116802+sazriel26@users.noreply.github.com>
2024-06-16 15:26:25 +0300

    [jira-cli] Initial import

---
+++ Formula/jira-cli.rb
@@ -0,0 +1,50 @@
+class JiraCli < Formula
+  desc "Jira CLI"
+  homepage "https://github.com/ankitpokhrel/jira-cli"
+  license "MIT"
+  version "1.5.1"
+  revision 1
+
+  url "https://github.com/ankitpokhrel/jira-cli.git"
+  livecheck do
+    url :head
+    strategy :github_latest
+  end
+
+  on_macos do
+    on_arm do
+      url "#{homepage}/releases/download/v#{version}/jira_#{version}_macOS_arm64.tar.gz"
+    end
+    on_intel do
+      url "#{homepage}/releases/download/v#{version}/jira_#{version}_macOS_x86_64.tar.gz"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "#{homepage}/releases/download/v#{version}/jira_#{version}_linux_arm64.tar.gz"
+    end
+    on_intel do
+      if Hardware::CPU.is_64_bit?
+        url "#{homepage}/releases/download/v#{version}/jira_#{version}_linux_x86_64.tar.gz"
+      else
+        url "#{homepage}/releases/download/v#{version}/jira_#{version}_linux_i386.tar.gz"
+      end
+    end
+  end
+
+  def install
+    # Only one binary to link
+    bin.install "bin/jira" => "jira-cli"
+  end
+
+  def caveats
+    <<~EOS
+      For more information, please kindly consult #{homepage}
+    EOS
+  end
+
+  test do
+    assert_match "#{version}", shell_output("#{bin}/jira-cli version")
+  end
+end
