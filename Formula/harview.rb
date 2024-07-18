53351479b778da9067b697d7f156fbd82c2f7927
Sharon Azriel <150116802+sazriel26@users.noreply.github.com>
2024-07-18 11:51:06 +0300

    [harview] Change install method + define version

--- Formula/harview.rb
+++ Formula/harview.rb
@@ -4,13 +4,15 @@ class Harview < Formula
   desc "CLI that dumps HAR to a human-readable summary"
   homepage "https://github.com/fboender/harview"
   license "MIT"
+  version "ad7d387"
 
   url "https://github.com/fboender/harview.git"
 
   uses_from_macos "python"
 
   def install
-    bin.install "src/harview"
-    rewrite_shebang detected_python_shebang(use_python_from_path: true), bin/"harview"
+    #bin.install "src/harview"
+    #rewrite_shebang detected_python_shebang(use_python_from_path: true), bin/"harview"
+    virtualenv_install_with_resources
   end
 end
