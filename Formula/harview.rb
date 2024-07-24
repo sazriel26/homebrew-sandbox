e5d1692d508705428930f57ba985a76371016d1b
Sharon Azriel <150116802+sazriel26@users.noreply.github.com>
2024-07-24 12:28:33 +0300

    [harview] no version, head only

--- Formula/harview.rb
+++ Formula/harview.rb
@@ -3,7 +3,6 @@ class Harview < Formula
 
   desc "CLI that dumps HAR to a human-readable summary"
   homepage "https://github.com/fboender/harview"
-  version "ad7d387"
   license "MIT"
 
   head "https://github.com/fboender/harview.git"
