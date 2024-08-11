b531a577ec38b1b318bfc01ee9436219f060f7d6
Sharon Azriel <150116802+sazriel26@users.noreply.github.com>
2024-08-11 14:29:19 +0300

    [xsos] Updated without patch usage

--- Formula/xsos.rb
+++ Formula/xsos.rb
@@ -14,36 +14,13 @@ class Xsos < Formula
   depends_on "gnu-getopt"
   depends_on "gnu-sed"
 
-  patch :DATA
-
   def install
-    bin.install "xsos"
+    libexec.install "xsos"
+
+    (bin/"xsos").write_env_script "#{HOMEBREW_PREFIX}/bin/bash", "-- #{libexec}/xsos",
+      :HOMEBREW_PREFIX => HOMEBREW_PREFIX,
+      :PATH => "${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnubin:${HOMEBREW_PREFIX}/opt/gnu-sed/libexec/gnubin:${HOMEBREW_PREFIX}/opt/gnu-getopt/bin:${HOMEBREW_PREFIX}/opt/gawk/libexec/gnubin:${PATH}"
 
     bash_completion.install "xsos-bash-completion.bash"
   end
 end
-__END__
-diff --git a/xsos b/xsos
-index b02e428..185cdc9 100755
---- a/xsos
-+++ b/xsos
-@@ -15,6 +15,18 @@
- #    General Public License <gnu.org/licenses/gpl.html> for more details.
- #-------------------------------------------------------------------------------
- 
-+# Homebrew GNU tools
-+if ! [[ "${PATH}" =~ ^HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin ]]; then
-+  export PATH="HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin:HOMEBREW_PREFIX/opt/gnu-getopt/bin:HOMEBREW_PREFIX/opt/gawk/libexec/gnubin:${PATH}"
-+fi
-+# Homebrew workaround for latest BASH
-+if ! [[ "${SHELL}" == "HOMEBREW_PREFIX/bin/bash" ]]; then
-+  exec /usr/bin/env SHELL="HOMEBREW_PREFIX/bin/bash" \
-+    "HOMEBREW_PREFIX/bin/bash" "${0}" "${@}"
-+  echo Oooops\! Looks like the workaround for Homebrew did not work. Exiting...>&2
-+  exit
-+fi
-+
- # See https://github.com/ryran/xsos/issues/208
- export LC_ALL=en_US.UTF-8
- 
-
