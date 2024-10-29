c0dc83171f4cea51256d49b823e2feea6ddaff0d
Sharon Azriel <150116802+sazriel26@users.noreply.github.com>
2024-10-29 17:12:50 +0200

    [ydb-cli] Rewriten formulas - 10/N

--- Formula/ydb-cli.rb
+++ Formula/ydb-cli.rb
@@ -12,9 +12,16 @@ class YdbCli < Formula
   on_macos do
     on_arm do
       version "2.13.0"
+      url "https://storage.yandexcloud.net/yandexcloud-ydb/release/#{version}/darwin/arm64/ydb", using: :nounzip
+      sha256 "9fd53c4ac2ca759cbf37e4cce2d7e531a7d0641124e003bd13e78da0b449c2c5"
+      livecheck do
+        skip "No more updated by upstream"
+      end
     end
     on_intel do
       version "2.15.0"
+      url "https://storage.yandexcloud.net/yandexcloud-ydb/release/#{version}/darwin/amd64/ydb", using: :nounzip
+      sha256 "c8b766d9ea4f28971b802a2462da471ac6dbb2a09c3c5820c079e0e493ebc42f"
     end
   end
 
@@ -22,14 +29,14 @@ class YdbCli < Formula
     on_intel do
       if Hardware::CPU.is_64_bit?
         version "2.15.0"
+        url "https://storage.yandexcloud.net/yandexcloud-ydb/release/#{version}/linux/amd64/ydb", using: :nounzip
+        sha256 "631edb9ed0851ae9c899bf91fb22f7ebe4c44b200aa5aa0c39877441e4133735"
       end
     end
   end
 
-  depends_on "ydb-cli@#{version}"
-
   def install
-    bin.install_symlink bin/"ydb.#{version}" => "ydb"
+    bin.install "ydb"
   end
 
   def caveats
