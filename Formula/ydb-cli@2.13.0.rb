273508540f89f5cf03491f69fe6d08f118b37a24
Sharon Azriel <150116802+sazriel26@users.noreply.github.com>
2024-10-29 13:24:29 +0200

    [ydb-cli] Version 2.15.0

---
+++ Formula/ydb-cli@2.13.0.rb
@@ -0,0 +1,39 @@
+class YdbCliAT2130 < Formula
+  desc "Yandex DB CLI"
+  homepage "https://ydb.tech"
+  version "2.13.0"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://storage.yandexcloud.net/yandexcloud-ydb/release/stable"
+    regex(/^(.+)$/i)
+  end
+
+  on_macos do
+    on_arm do
+      url "https://storage.yandexcloud.net/yandexcloud-ydb/release/#{version}/darwin/arm64/ydb", using: :nounzip
+    end
+    on_intel do
+      url "https://storage.yandexcloud.net/yandexcloud-ydb/release/#{version}/darwin/amd64/ydb", using: :nounzip
+    end
+  end
+
+  on_linux do
+    on_intel do
+      if Hardware::CPU.is_64_bit?
+        url "https://storage.yandexcloud.net/yandexcloud-ydb/release/#{version}/linux/amd64/ydb", using: :nounzip
+      end
+    end
+  end
+
+  def install
+    # Only one binary to link
+    bin.install "ydb"
+  end
+
+  def caveats
+    <<~EOS
+      For more information, please kindly consult #{homepage}/en/docs/reference/ydb-cli/
+    EOS
+  end
+end
--- Formula/ydb-cli@2.15.0.rb
+++ Formula/ydb-cli@2.15.0.rb
@@ -1,7 +1,7 @@
-class YdbCli < Formula
+class YdbCliAT2150 < Formula
   desc "Yandex DB CLI"
   homepage "https://ydb.tech"
-  #version ""
+  version "2.15.0"
   license "Apache-2.0"
 
   livecheck do
@@ -10,21 +10,17 @@ class YdbCli < Formula
   end
 
   on_macos do
-    on_arm do
-      version "2.13.0"
-      url "https://storage.yandexcloud.net/yandexcloud-ydb/release/#{version}/darwin/arm64/ydb", using: :nounzip
-    end
     on_intel do
-      version "2.14.0"
       url "https://storage.yandexcloud.net/yandexcloud-ydb/release/#{version}/darwin/amd64/ydb", using: :nounzip
+      sha256 "c8b766d9ea4f28971b802a2462da471ac6dbb2a09c3c5820c079e0e493ebc42f"
     end
   end
 
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        version "2.14.0"
         url "https://storage.yandexcloud.net/yandexcloud-ydb/release/#{version}/linux/amd64/ydb", using: :nounzip
+        sha256 "631edb9ed0851ae9c899bf91fb22f7ebe4c44b200aa5aa0c39877441e4133735"
       end
     end
   end
